9e4d4ce721d749de95196ebb4f450497e6c571a8
isxrh <ronghui.xie@foxmail.com>
2024-07-11 16:17:50 +0800

    add search module using elasticsearch (6.6)

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -56,6 +56,11 @@ public interface CommunityConstant {
      */
     String TOPIC_FOLLOW = "follow";
 
+    /**
+     * 主题：发帖
+     */
+    String TOPIC_PUBLISH = "publish";
+
 
     /**
      * 系统用户的ID
