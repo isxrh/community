66602e803ade18ef17118d3efae8daa159669882
isxrh <ronghui.xie@foxmail.com>
2024-07-11 21:55:25 +0800

    add set top,wonderful,delete module(7.5)

--- src/main/java/com/nowcoder/community/config/SecurityConfig.java
+++ src/main/java/com/nowcoder/community/config/SecurityConfig.java
@@ -46,6 +46,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements Comm
                         AUTHORITY_USER
 
                 )
+                .antMatchers(
+                       "/discuss/top",
+                       "/discuss/wonderful"
+                ).hasAnyAuthority(
+                        AUTHORITY_MODERATOR
+                )
+                .antMatchers(
+                        "/discuss/delete"
+                ).hasAnyAuthority(
+                        AUTHORITY_ADMIN
+                )
                 .anyRequest().permitAll()
                 .and().csrf().disable();
         // 权限不够时的处理
